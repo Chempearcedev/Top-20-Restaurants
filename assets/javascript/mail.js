c1938211ca66f235f5a805a863ef7456d67d92e9
Chempearcedev <chempearce1@hotmail.com>
2021-02-01 21:56:05 +0000

    added function for form to send emails

--- assets/javascript/mail.js
+++ assets/javascript/mail.js
@@ -1,16 +1,15 @@
 function sendMail(contactForm) {
-    emailjs.send("service_8jzchbj", "restaurant-finder", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+    emailjs.send("service_8jzchbj","restaurant-finder", {
+     "message": contactForm.message.value,
+     "from_name": contactForm.name.value,
+     "from_email": contactForm.emailaddress.value,
     })
     .then(
-        function(response) {
-            console.log("Success", response);
-        },
-        function(error) {
-            console.log("Error", error);
-        }
-    );
-    return false;  
+        function (response){
+        console.log("Success", response);
+    },
+    function(error) {
+        console.log("Error", error)
+    });
+    return false;
 }
