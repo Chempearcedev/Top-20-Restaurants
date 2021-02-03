12c3fc6e3f1ba51b529d94695d86deb24f383254
Chempearcedev <chempearce1@hotmail.com>
2021-02-03 21:34:49 +0000

    placed map locations into an array

---
+++ assets/javascript/map.js
@@ -0,0 +1,260 @@
+const locations = [
+    {
+        name: "Etch",
+        lat: 50.82846,
+        lng:  -0.177569,
+        address: "216 Church Road, Hove BN3 2DJ England",
+        website: "https://www.etchfood.co.uk/home",
+        priceRange: "£50 - £150",
+        cuisine: "French, European, British",
+        rating: "4.5"
+    },
+    {
+        name: "Salt Room",
+        lat: 50.8214619,
+        lng: -0.1483314,
+        address: "216 Church Road, Hove BN3 2DJ England",
+        website: "https://www.etchfood.co.uk/home",
+        priceRange: "£50 - £150",
+        cuisine: "French, European, British",
+        rating: "4.5"
+    },
+
+     {
+        name: "The Chilli Pickle",
+        lat:  50.824828,
+        lng: -0.138905,
+        address: "17 Jubilee Street, Brighton BN1 1GE England",
+        website: "https://www.thechillipickle.com",
+        priceRange: "£7 - £28",
+        cuisine: "Indian, Asian",
+        rating: "4.0"
+    },
+   {
+        name: "Riddle And Finns",
+        lat:  50.822055,
+        lng: -0.140776,
+        address: "Arch 139, Brighton BN1 2FN England",
+        website: "https://www.riddleandfinns.co.uk",
+        priceRange: "£29 - £72",
+        cuisine: "British, Seafood ",
+        rating: "4.5"
+    },
+       {
+        name: "Terre a Terre",
+        lat:  50.820397,
+        lng: -0.139095,
+        address: "71 East Street, Brighton BN1 1HQ England",
+        website: "https://www.terreaterre.co.uk",
+        priceRange: "£35 - £60",
+        cuisine: "International, British, Healthy",
+        rating: "4.5"
+    },
+          {
+        name: "Gingerman",
+        lat:  50.824114,
+        lng: -0.155105,
+        address: " 21A Norfolk Square, Brighton BN1 2PD England",
+        website: "https://www.gingermanrestaurant.com",
+        priceRange: " £15 - £90",
+        cuisine: "European, British",
+        rating: "4.5"
+    },
+            {
+        name: "The Little Fish Market",
+        lat:  50.82503,
+        lng: -0.157086,
+        address: " 10 Upper Market Street, Hove BN3 1AS England",
+        website: "https://www.thelittlefishmarket.co.uk",
+        priceRange: " £15 - £90",
+        cuisine: "Seafood, European, British",
+        rating: "5.0"
+    },
+             {
+        name: "64 Degrees",
+        lat:  50.822055,
+        lng: -0.140776,
+        address: "53 Meeting House Lane, Brighton BN1 1HB England",
+        website: "https://www.64degrees.co.uk",
+        priceRange: "£15 - £40",
+        cuisine: "Seafood, European, British",
+        rating: "4.5"
+    },
+                 {
+        name: "The Urchin",
+        lat:  50.826636,
+        lng: -0.161594,
+        address: "15-17 Belfast Street, Hove BN3 3YS England",
+        website: "https://www.urchinpub.co.uk",
+        priceRange: " £10 - £25",
+        cuisine: "Seafood, British, Pub",
+        rating: "4.5"
+    },
+                  {
+        name: "Etci Kitchen",
+        lat:  50.823933,
+        lng: -0.151023,
+        address: "47 Preston Street, Brighton, BN1 2HP England",
+        website: "https://www.etcikitchen.com",
+        priceRange: " £5 - £65",
+        cuisine: "Seafood, European, British",
+        rating: "5.0"
+    },
+                      {
+        name: "Chard",
+        lat:  50.825497,
+        lng: -0.158387,
+        address: "31A Western Road, Hove BN3 1AF England",
+        website: "www.chardrestaurant.uk",
+        priceRange: " £6 - £25",
+        cuisine: "British",
+        rating: "5.0"
+    },
+                  {
+        name: "Isaac At Gloucester Street",
+        lat:  50.827198,
+        lng: -0.13695,
+        address: "2 Gloucester Street, Brighton BN1 4EW England",
+        website: "www.isaac-at.com",
+        priceRange: "£35 - £49",
+        cuisine: "British",
+        rating: "4.5"
+    },
+               {
+        name: "The Ginger Dog",
+        lat:  50.819444,
+        lng: -0.122266,
+        address: "12 College Place, Brighton BN2 1HN England",
+        website: "www.thegingerdog.com",
+        priceRange: "£21 - £38",
+        cuisine: "Bar, British, Pub",
+        rating: "4.0"
+    },
+           {
+        name: "Cin Cin",
+        lat:  50.826126,
+        lng: -0.137362,
+        address: "13-16 Vine Street, Brighton BN1 4AG England",
+        website: "www.cincin.co.uk",
+        priceRange: "£7 - £22",
+        cuisine: "Contemporary, Dining bars",
+        rating: "4.5"
+    },
+      {
+        name: "The Set",
+        lat:  50.82326,
+        lng: -0.149985,
+        address: "33 Regency Square, Brighton BN1 2GG England",
+        website: "www.thesetrestaurant.com",
+        priceRange: "£75 - £170",
+        cuisine: "British, International",
+        rating: "4.5"
+    },
+    {
+        name: "The Flint House",
+        lat: 50.822321,
+        lng: -0.139956,
+        address: "13 Hanningtons Lane, Brighton BN1 1GS England",
+        website: "www.thesetrestaurant.com",
+        cuisine: "British, Wine Bar, European",
+        rating: "4.5"
+    },
+       {
+        name: "Murmur",
+        lat: 50.821025,
+        lng: -0.148356,
+        address: "91-96 Kings Road Arches, Brighton BN1 2FN England",
+        website: "www.murmur-restaurant.co.uk",
+        priceRange: "£4 - £13",
+        cuisine: "British, Seafood, European, Contemporary",
+        rating: "4.5"
+    },
+     {
+        name: "Baqueano",
+        lat: 50.825148,
+        lng: -0.153375,
+        address: "125 Western Road, Brighton BN1 2AD England",
+        website: "www.baqueano.co.uk",
+        priceRange: "£7 - £40",
+        cuisine: "Steakhouse, Grill, Argentinian",
+        rating: "5.0"
+    },
+    {
+        name: "Bincho Yakitori",
+        lat: 50.824016,
+        lng: -0.15058,
+        address: "63 Preston Street, Brighton BN1 2HE England",
+        website: "www.binchoyakitori.com",
+        priceRange: "£12 - £50",
+        cuisine: "Japanese, Asian",
+        rating: "4.5"
+    },
+       {
+        name: "Petit Pois Restaurant",
+        lat: 50.821384,
+        lng: -0.15058,
+        address: "70 Ship Street, Brighton BN1 1AE England",
+        website: "www.petitpoisbrighton.co.uk",
+        priceRange: "£10 - £15",
+        cuisine: "French, European",
+        rating: "4.5"
+    }
+]
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: {
+            lat: 50.82846,
+            lng: -0.177569
+        },
+          zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER,
+      },
+         streetViewControl: {
+        position: google.maps.ControlPosition.BOTTOM_CENTER 
+      },
+
+    });
+
+     const bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(map);
+
+    const labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+    let markers = locations.map(function(location, i) {
+        let marker = new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length],
+            animation: google.maps.Animation.DROP,
+        });
+
+        const infowindow = new google.maps.InfoWindow({
+          content: `
+            <div id="content">
+                <div id="siteNotice"></div>
+                <h1 id="firstHeading" class="firstHeading">${location.name}</h1>
+                <div id="bodyContent">
+                    <p><span class="bold">Rating</span>:</b> ${location.rating}</p>
+                    <p>Address: ${location.address}</p>
+                    <p>Price Range: ${location.priceRange}</p>
+                    <p>Cuisine:</b> ${location.cuisine}</p>
+                    <a href="${location.website}" target="_blank">
+                        ${location.website}
+                    </a>
+                </div>
+            </div>
+          `
+        });
+
+        marker.addListener("click", () => {
+          infowindow.open(map, marker);
+        });
+
+        return marker;
+    });
+
+    const markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
